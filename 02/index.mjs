30609c389a33f6228beb88c751255e19500784e8
Dennis Collon <dennis.collon@secucloud.com>
2020-12-02 10:04:52 +0100

    day2

---
+++ 02/index.mjs
@@ -0,0 +1,49 @@
+import { readFilecontentsToArray } from "../utils/fileReader.mjs";
+import { dirname, resolve } from "path";
+import { fileURLToPath } from "url";
+
+const inputPath = resolve(
+  dirname(fileURLToPath(import.meta.url)),
+  "./",
+  "input.txt"
+);
+
+const input = readFilecontentsToArray(inputPath);
+
+const getPolicyParts = (policy) => {
+  const [minAndMax, charwithColon, password] = policy.split(" ");
+  const [min, max] = minAndMax.split("-");
+
+  return { min, max, char: charwithColon.charAt(0), password };
+};
+
+const part1 = () => {
+  const validPasswords = input.filter((policy) => {
+    const { min, max, char, password } = getPolicyParts(policy);
+    const occurences = password.split(char).length - 1;
+    return occurences >= min && occurences <= max;
+  });
+  return validPasswords.length;
+};
+
+const part2 = () => {
+  const validPasswords = input.filter((policy) => {
+    const { min: position1, max: position2, char, password } = getPolicyParts(
+      policy
+    );
+
+    const firstPosition = position1 - 1;
+    const secondPosition = position2 - 1;
+
+    return (
+      (password.charAt(firstPosition) === char &&
+        password.charAt(secondPosition) !== char) ||
+      (password.charAt(firstPosition) !== char &&
+        password.charAt(secondPosition) === char)
+    );
+  });
+  return validPasswords.length;
+};
+
+console.log({ part1: part1() });
+console.log({ part2: part2() });
